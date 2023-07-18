b9601ffd33ad4adf065785a4a8b2a791df14a36a
Mapalo04 <lukundomapalo@gmail.com>
2023-07-18 04:37:00 -0700

    added 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * int_abs - prints the sign of a number.
+ * @n: is int
+ *
+ * Return: 1 if alpha else 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+{
+		c = c * -1;
+		return (c);
+}
+	else
+{
+		return (c);
+}
+	return (0);
+}
