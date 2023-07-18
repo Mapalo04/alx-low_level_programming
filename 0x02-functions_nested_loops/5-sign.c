6fe696ecca7cd43720d5cbec9f2e5b850c125cc8
Mapalo04 <lukundomapalo@gmail.com>
2023-07-18 03:23:21 -0700

    added 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number.
+ * @n: is int
+ *
+ * Return: 1 if alpha else 0
+ */
+
+int print_sign(int n)
+{
+	if (c > 0)
+{
+		putchar('+');
+		return (1);
+}
+	else if (c == 0)
+{
+		putchar('0');
+		return (0);
+}
+	else if (c < 0)
+{
+		putchar('-');
+		return (-1);
+}
+}
+
