f48bc9eaca339e410c93ed2f9f2ea7336f32001d
Mapalo04 <lukundomapalo@gmail.com>
2023-07-18 06:30:56 -0700

    added 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - prints the times table.
+ *
+ * Return: 0 on success
+ */
+void times_table(void)
+{
+	int rows = 0;
+	int clm = 0;
+
+	while (rows < 10)
+{
+		while (clm < 10)
+{
+			int res = (rows * clm);
+
+			if (res < 10)
+{
+				_putchar(' ');
+				_putchar('0' + res);
+}
+			else if (res >= 10)
+{
+				_putchar('0' + (res / 10));
+				_putchar('0' + (res % 10));
+}
+			if (clm != 9)
+{
+				_putchar(',');
+				_putchar(' ');
+}
+			clm++;
+}
+		_putchar('\n');
+		rows++;
+		clm = 0;
+}
+}
