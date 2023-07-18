20d36dd4574bcbc4f5c24155cb6298f25b6c10ef
Mapalo04 <lukundomapalo@gmail.com>
2023-07-18 06:47:18 -0700

    added 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds to numbers.
+ * @a: is int
+ * @b: is int
+ *
+ * Return: 1 if alpha else 0
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+	return(sum);    
+}
