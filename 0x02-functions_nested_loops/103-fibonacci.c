7ec6379b3d653134a76e7271c87ad1e78b96af82
Mapalo04 <lukundomapalo@gmail.com>
2023-07-18 12:01:52 -0700

    added 3-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long int cur = 1;
+	long int pre;
+	long int forw = 2;
+	int mult;
+	int sum;
+
+	for (mult = 1; mult <= 50; mult++)
+{
+		if (cur % 2 == 0)
+{
+		sum = sum + cur;
+}
+		pre = cur;
+		cur = forw;
+		forw = pre + cur;
+		if (cur > 4000000)
+{
+			printf("%ld", sum);
+			break;
+}
+}
+	printf("\n");
+	return (0);
+}
