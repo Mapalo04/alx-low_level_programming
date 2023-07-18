a6baa89a8363ad9308380a99cafe7cd661ea6206
Mapalo04 <lukundomapalo@gmail.com>
2023-07-18 07:34:35 -0700

    added 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,14 +3,14 @@
 
 /**
  * print_to_98 - prints natural numbers from n to 98.
- * @n: is int
+ * @a: is int
  *
  * Return: 0 on success.
  */
 
 void print_to_98(int a)
 {
-	while ( a <= 98 || a >= 98)
+	while (a <= 98 || a >= 98)
 {
 		printf("%d", a);
 		if (a != 98)
