a350446ec72dfb7f20ff3e3c888609828622ff76
Mapalo04 <lukundomapalo@gmail.com>
2023-07-20 04:10:58 -0700

    updated 5-more_numbers,c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,7 +15,15 @@ void more_numbers(void)
 {
 		while (num < 15)
 {
-			_putchar('0' + num);
+			if  (num < 10)
+{
+				_putchar('0' + num);
+}
+			else if (num > 10)
+{
+				_putchar('0' + (num / 10));
+				_putchar('0' + (num % 10));
+}
 			num++;
 }
 	_putchar('\n');
