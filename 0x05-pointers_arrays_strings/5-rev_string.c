0361dea015aeb75ecff12c135b0ca050701a9c56
Mapalo04 <lukundomapalo@gmail.com>
2023-07-24 08:28:39 -0700

    added 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+/**
+ * rev_string - prints a string in revers followed by a new line.
+ * @s: is of type char
+ *
+ */
+
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int count = 0;
+	int i;
+
+	while (s[count] != '\0')
+{
+	start++;
+}
+	i = 0;
+	while (i < count)
+{
+	count--;
+	rev = s[i];
+	s[i] = s[counter];
+	s[counter] = rev;
+	i++;
+}
+}
