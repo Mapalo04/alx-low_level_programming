a40e7a88ac5761bc0a56c27c84f1501aa7595a4a
Mapalo04 <lukundomapalo@gmail.com>
2023-07-24 04:33:35 -0700

    added 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * int _strlen - returns the length of a string
+ * @s: is a pointer
+ *
+ * Returns - length of a string.
+ */
+
+int _strlen(char *s)
+{
+	long unsigned int a = sizeof(*s);
+
+	return(a);
+}
