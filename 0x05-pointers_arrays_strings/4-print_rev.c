469b56dfe19ba3f3656635de5149a3276fc314e8
Mapalo04 <lukundomapalo@gmail.com>
2023-07-24 06:02:10 -0700

    added 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+ * _puts - prints a string followed by a new line.
+ * @str: is of type char
+ *
+ */
+
+void _puts(char *str)
+{
+	int n = 0;
+	int start;
+	while (*str )
+{
+	n++;
+	str++;
+}
+	s--;
+	start = n;
+	while (start > 0)
+{
+	
+	_putchar(*s);
+	s--;
+}
+	_putchar('\n');
+}
