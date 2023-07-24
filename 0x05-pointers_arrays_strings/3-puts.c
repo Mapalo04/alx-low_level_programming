4deba147a1024f2274265863337724e4bbeb5d69
Mapalo04 <lukundomapalo@gmail.com>
2023-07-24 05:13:17 -0700

    added 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * _puts - prints a string followed by a new line.
+ * @str: is of type char
+ *
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+{
+	_putchar(*str++)
+}
+	_putchar('\n');
+}
