7e4a402d9de1609d20c8b77dee9d46672b3f7012
Mapalo04 <lukundomapalo@gmail.com>
2023-07-25 09:13:23 +0000

    added 101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,35 @@
+#include <stdio>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * 
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int pass[100];
+	int i, sum, n;
+
+	sum = 0;
+
+	srand(time(NuLL));
+
+	i = 0;
+	while (i < 100)
+{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+}
+		i++;
+}
+	return (0);
+}
