e192e25e17b231b410e3266fb3a9b937c25fdd48
Mapalo04 <lukundomapalo@gmail.com>
2023-07-27 21:12:43 +0000

    added 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,17 +12,16 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	unsigned int num;
+	int check = 0;
+	int num1, num2;
 
-	num = n;
-	if (n < 0)
-{
-		_putchar(' ');
-		num = -num;
-}
-	if (num / 10 != 0)
-{
-		print_number(num / 10);
-}
-	_putchar((num % 10) + '0');
+	for (check = 0; n1[check] != '\0'; check++)
+		num1 = num1 * 10 + n1[check];
+	for (check = 0; n2[check] != '\0'; check++)
+		num2 = num2 * 10 + n2[check];
+	*r = num1 + num2;
+	if (*r == size_r)
+		return (0);
+	return (r);	
+
 }
