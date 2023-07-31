657ecd7a7e834454abea003367d17905269e5af8
Mapalo04 <lukundomapalo@gmail.com>
2023-07-31 10:14:10 +0000

    added 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,14 +6,14 @@
  * @b: char parameter to replace the memory with
  * @n: number to replace
  *
- * Returns: the updated memory area
+ * Return: the updated memory area
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int a = 0;
+	int a;
 
-	for (; 0 < n; a++)
+	for (a = 0; n > 0; a++)
 	{
 		s[a] = b;
 		n--;
