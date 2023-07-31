008f75639b390b2824128b9ce9ee2875f82a12cd
Mapalo04 <lukundomapalo@gmail.com>
2023-07-31 14:23:46 +0000

    added 101-crackme_password and 100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -3,7 +3,7 @@
 /**
  * set_string - sets the value of a pointer to a char
  * @s: the pointer
- * @to: the character to set to 
+ * @to: the character to set to
  */
 void set_string(char **s, char *to)
 {
