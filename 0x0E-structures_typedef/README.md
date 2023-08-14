adf1db8d942bb26a88d6b526104e14726c9b519f
Mapalo04 <lukundomapalo@gmail.com>
2023-08-14 09:11:27 +0000

    added dog.h and README

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This directory will contain scripts on structures and typedefs.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * dog - contains info about a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: onwer's name
+ *
+ */
+
+struct dog{
+	char *name;
+	float age;
+	char *name;
+}
