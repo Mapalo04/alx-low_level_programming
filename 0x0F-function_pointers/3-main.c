0e7aa3f258b26b446412344ca7901aea00aaa6f0
Mapalo04 <lukundomapalo@gmail.com>
2023-08-16 21:06:51 +0000

    updated 3-op_functions, 3-calc.h and 3-get_op_func.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,7 +16,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	int num1, num2;
 	char *op;
 
-        if (argc != 4)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
@@ -29,15 +29,14 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
-                exit(99);
-        }
+		exit(99);
+	}
 
-        if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-
 	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }
