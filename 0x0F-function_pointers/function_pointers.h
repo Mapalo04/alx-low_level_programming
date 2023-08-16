fcac679e638e74c7f9311be493e26628a8f5bc1a
Mapalo04 <lukundomapalo@gmail.com>
2023-08-16 16:36:15 +0000

    added

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,7 @@
 #ifndef _function_pointers_h_
 #define _function_pointers_h_
+#include <stddef.h>
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
