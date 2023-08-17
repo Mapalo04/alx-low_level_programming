2fdc5534aedf18b4ed4ed2b87878c5cb8128c4ea
Mapalo04 <lukundomapalo@gmail.com>
2023-08-17 17:48:31 +0000

    added 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -30,6 +30,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (i < n - 1 && separator != NULL)
 			printf("%s ", separator);
 	}
-	putchar('\n');
+	printf("\n");
 	va_end(strs);
 }
