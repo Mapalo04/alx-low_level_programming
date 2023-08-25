4f87bdac700a9773c97d407e68d57c5e85a106bb
Mapalo04 <lukundomapalo@gmail.com>
2023-08-25 10:15:04 -0700

    updated 0-print_lis lists.h

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,10 +12,10 @@ size_t print_list(const list_t *h)
 {
 	size_t a = 0;
 
-	while(h)
+	while (h)
 	{
 		if (!h->str)
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		else
 		{
 			printf("[%u] %s\n", h->len, h->str);
