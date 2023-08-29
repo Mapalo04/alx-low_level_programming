cfb4a771e05ba8f569ee6d17efd4882dcb22e744
Mapalo04 <lukundomapalo@gmail.com>
2023-08-29 20:41:31 +0000

    added 8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- * sum_listint - sums up all the data in the list. 
+ * sum_listint - sums up all the data in the list.
  * @head: pointer to the first node in the list
  *
  * Return: sum of all the data else 0;
